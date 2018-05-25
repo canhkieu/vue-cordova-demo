acfdbd0f6e55784d8b016c919c40078a643c9789
Canh Kieu <canhkieu@azwebplus.com>
2018-05-25 16:46:04 +0700

    Create project

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  lintOnSave: true,
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === "production") {
+      // mutate config for production...
+    } else {
+      // mutate for development...
+    }
+  }
+};
